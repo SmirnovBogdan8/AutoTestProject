7bf3705e0641b6573e5900d6203f4fe735341c0e
smirn <smirnovbogdan8@gmail.com>
2025-08-03 20:04:28 +0300

    Локаторы поиск по организациям

--- src/test/java/elibrary/locators/OrganizationsLocators.java
+++ src/test/java/elibrary/locators/OrganizationsLocators.java
@@ -1,4 +1,17 @@
 package elibrary.locators;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class OrganizationsLocators {
+    public static final SelenideElement TXT_QUERY = $("//input[@id='query']");
+    public static final SelenideElement SELECT_TOWN = $("//select[@class='input' and @name='town']");
+    public static final SelenideElement SELECT_REGION_ID = $("//select[@class='input' and @name='regionid']");
+    public static final SelenideElement SELECT_COUNTRY_ID = $("//select[@class='input' and @name='countryid']");
+    public static final SelenideElement SELECT_SORT_ORDER = $("//select[@class='input' and @id='sortorder']");
+    public static final SelenideElement SELECT_ORDER = $("//select[@class='input' and @id='order']");
+
+    public static final SelenideElement BTN_RESET_FROM = $("//a[text()='Очистить']");
+    public static final SelenideElement BTN_SEARCH = $("//a[text()='Поиск']");
 }
