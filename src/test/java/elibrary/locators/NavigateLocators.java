7e7901bfb9ebd538f23be5c32ea69b006e86c255
smirn <smirnovbogdan8@gmail.com>
2025-08-03 19:47:29 +0300

    Локаторы главная страница

--- src/test/java/elibrary/locators/NavigateLocators.java
+++ src/test/java/elibrary/locators/NavigateLocators.java
@@ -1,4 +1,17 @@
 package elibrary.locators;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class NavigateLocators {
+    public static final SelenideElement LINK_MAGAZINES = $("//a[text()='ЖУРНАЛЫ']");
+    public static final SelenideElement LINK_BOOKS = $("//a[text()='КНИГИ']");
+    public static final SelenideElement LINK_PATENTS = $("//a[text()='ПАТЕНТЫ']");
+    public static final SelenideElement LINK_SEARCH = $("//a[text()='ПОИСК']");
+    public static final SelenideElement LINK_AUTHORS = $("//a[text()='АВТОРЫ']");
+    public static final SelenideElement LINK_ORGANIZATIONS = $("//a[text()='ОРГАНИЗАЦИИ']");
+    public static final SelenideElement LINK_KEY_WORDS = $("//a[text()='КЛЮЧЕВЫЕ СЛОВА']");
+    public static final SelenideElement LINK_RUBRICS = $("//a[text()='РУБРИКАТОР']");
+    public static final SelenideElement LINK_MAIN_PAGE = $("//a[text()='Начальная страница']");
 }
