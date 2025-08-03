c037733cfc451db358123332931e09406fcae403
smirn <smirnovbogdan8@gmail.com>
2025-08-03 18:48:13 +0300

    Локаторы поиск по ключевым словам

--- src/test/java/elibrary/locators/AuthorsLocators.java
+++ src/test/java/elibrary/locators/AuthorsLocators.java
@@ -18,7 +18,7 @@ public class AuthorsLocators {
     public static final SelenideElement SELECT_THEME = $("//select[@id='rubriccode' and @name='rubriccode']");
     public static final SelenideElement SELECT_METRICS = $("//select[@class='input' and @name='metrics']");
     public static final SelenideElement CHECKBOX_SHOW_NOPUBL = $("//input[@type='checkbox' and @id='show_nopubl']");
-    public static final SelenideElement SELECT_SORTORDER = $("//select[@id='sortorder' and @name='sortorder']");
+    public static final SelenideElement SELECT_SORT_ORDER = $("//select[@id='sortorder' and @name='sortorder']");
     public static final SelenideElement SELECT_ORDER = $("//select[@id='order' and @name='order']");
     public static final SelenideElement LINK_AUTHORS_TUTORIAL = $("//a[text()='Инструкция для авторов']");
     public static final SelenideElement BTN_RESET_FROM = $("//div[@class='butblue' and text()='Очистить']");
--- src/test/java/elibrary/locators/KeywordsLocators.java
+++ src/test/java/elibrary/locators/KeywordsLocators.java
@@ -1,4 +1,14 @@
 package elibrary.locators;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class KeywordsLocators {
+    public static final SelenideElement TXT_BOOK_NAME = $("//input[@id='qwd']");
+    public static final SelenideElement SELECT_METRICS = $("//select[@class='input' and @name='metrics']");
+    public static final SelenideElement SELECT_SORT_ORDER = $("//select[@id='sortorder' and @name='sortorder']");
+    public static final SelenideElement SELECT_ORDER = $("//select[@id='order' and @name='order']");
+    public static final SelenideElement BTN_RESET_FROM = $("//div[@class='butblue' and text()='Очистить']");
+    public static final SelenideElement BTN_SEARCH = $("//div[@class='butred' and text()='Поиск']");
 }
