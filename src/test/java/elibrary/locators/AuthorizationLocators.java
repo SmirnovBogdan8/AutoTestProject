19e9aafe8b79aeb22311c3f76efca8e96045907f
smirn <smirnovbogdan8@gmail.com>
2025-08-03 09:03:51 +0300

    tmp

---
+++ src/test/java/elibrary/locators/AuthorizationLocators.java
@@ -0,0 +1,8 @@
+package elibrary.locators;
+
+import com.codeborne.selenide.SelenideElement;
+
+public class AuthorizationLocators {
+    // Локаторы элементов страницы
+    public static final SelenideElement INPUT_TXT = null;
+}
