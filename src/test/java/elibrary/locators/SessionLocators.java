a2466622855c9197ac6730663180db0a3ace66ca
smirn <smirnovbogdan8@gmail.com>
2025-08-04 10:33:58 +0300

    Локаторы поиска

--- src/test/java/elibrary/locators/SessionLocators.java
+++ src/test/java/elibrary/locators/SessionLocators.java
@@ -1,4 +1,14 @@
 package elibrary.locators;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class SessionLocators {
+    public static final SelenideElement LABLE_USER_NAME = $("");
+    public static final SelenideElement LABLE_BEGIN_SESSION = $("");
+    public static final SelenideElement LABLE_TIME_SESSION = $("");
+    public static final SelenideElement LINK_ACCOUNT = $("Личный кабинет");
+    public static final SelenideElement LINK_CLOSE_SESSION = $("Закрыть сессию");
+
 }
